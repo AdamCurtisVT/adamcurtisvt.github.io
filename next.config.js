3d61f8d385d414930bb024db67dcaa1321cfbbad
Adam Curtis <adamcurtisvt@gmail.com>
2022-02-08 08:41:13 -0500

    Update

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/adamcurtisvt/' : ''
+  assetPrefix: isProd ? '/adamcurtisvt.github.io/' : ''
 }
